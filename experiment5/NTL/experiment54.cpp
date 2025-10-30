0d9caad32b909908358d38039d8104c6ca2bcf87
lyj <2871148635@qq.com>
2025-10-30 21:25:31 +0800

    pollard_rho 算法

---
+++ experiment5/NTL/experiment54.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<NTL/ZZ.h>
+#include<NTL/vector.h>
+#include<map>
+using namespace std;
+using namespace NTL;
+
+
+int main(){
+    ZZ n(36);
+    Vec<ZZ> pr;
+    //factor(pr,n);
+
+}
